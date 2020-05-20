6b04d35dc706b166fc8387e9cc02ed09bb546aaa
artem <salavursa@yandex.ru>
2020-05-20 13:17:18 +0300

    done

--- src/main/java/ru/ifmo/collections/FirstUnique.java
+++ src/main/java/ru/ifmo/collections/FirstUnique.java
@@ -1,19 +1,40 @@
 package ru.ifmo.collections;
 
+import java.util.ArrayList;
+
 /**
  * Design a class which contains integers and returns first unique integer (in order of addition).
  * FirstUniqueTest can be used as an example.
  */
 public class FirstUnique {
+    ArrayList<Integer> numbers;
+
     public FirstUnique(int[] numbers) {
-        // TODO implement
+        this.numbers = new ArrayList<>();
+        for (int i = 0; i < numbers.length; i++) {
+            this.numbers.add(numbers[i]);
+        }
     }
 
     public int showFirstUnique() {
-        throw new UnsupportedOperationException(); // TODO implement
+        ArrayList<Integer> uniq = new ArrayList<>();
+        ArrayList<Integer> paired = new ArrayList<>();
+
+        numbers.forEach(number->{
+            if (uniq.contains(number)) {
+                uniq.remove(number);
+                paired.add(number);
+            }
+            else {
+                if (!paired.contains(number)) uniq.add(number);
+            }
+        });
+
+        if (uniq.size()<1) return -1;
+        return uniq.get(0);
     }
 
     public void add(int value) {
-        throw new UnsupportedOperationException(); // TODO implement
+        this.numbers.add(value);
     }
 }
